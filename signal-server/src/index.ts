09ad8847cdde3f208da965d0439906851f56c14b
Mh-Alim <workingalim@gmail.com>
2024-12-16 14:39:08 +0530

    signaling server offer and answer flow done

--- signal-server/src/index.ts
+++ signal-server/src/index.ts
@@ -1,7 +1,24 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 5000 });
 
+let senderSocket: WebSocket | null = null;
+let receiverSocket: WebSocket | null = null;
 wss.on("connection", (ws) => {
-    ws.on
-})
+  console.log("socket connected");
+
+  ws.on("message", (data: any) => {
+    if (data.type === "sender") return (senderSocket = ws);
+    if (data.type === "receiver") return (receiverSocket = ws);
+
+    if (data.type === "offer") {
+      if (ws !== senderSocket) return;
+      return receiverSocket?.send(data);
+    }
+
+    if (data.type === "answer") {
+      if (ws !== receiverSocket) return;
+      return senderSocket?.send(data);
+    }
+  });
+});
